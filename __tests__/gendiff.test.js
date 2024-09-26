1c8795b6c8aa95d651261832ac13a32ddd4c3532
Shi0-X <jshi0x@gmail.com>
2024-09-26 13:52:00 -0500

    Modificacion duplicado en pruebas

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,17 +1,20 @@
-import genDiff from '../src/index.js';
+import genDiff from '../src/index.js'; 
 import getFixturePath from '../src/getFixturePath.js';
 import jsonResult from '../__fixtures__/jsonResult.js'; // Importamos jsonResult
 
-// Prueba para JSON
+// Función para realizar las pruebas con distintos formatos de archivo
+const testDiff = (format) => {
+  const file1 = getFixturePath(`file1.${format}`);
+  const file2 = getFixturePath(`file2.${format}`);
+  expect(genDiff(file1, file2, format)).toEqual(jsonResult);
+};
+
+// Prueba para archivos JSON
 test('should return correct diff for two JSON files', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-  expect(genDiff(file1, file2, 'json')).toEqual(jsonResult);
+  testDiff('json');
 });
 
-// Prueba para YAML
+// Prueba para archivos YAML
 test('should return correct diff for two YAML files', () => {
-  const file1 = getFixturePath('file1.yml');
-  const file2 = getFixturePath('file2.yml');
-  expect(genDiff(file1, file2, 'yml')).toEqual(jsonResult);
+  testDiff('yml');
 });
