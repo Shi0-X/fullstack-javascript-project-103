c773b0052653eda7842fdaa4e3ac2ae9b163af34
Shi0-X <jshi0x@gmail.com>
2024-10-11 10:14:00 -0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,20 @@
 const formatNode = (node, depth) => {
   const key = node.key;
+  const indent = '  '.repeat(depth - 1); // Resta un nivel de profundidad
 
   switch (node.type) {
-  case 'added':
-    return `${'    '.repeat(depth)}+ ${key}: ${formatValue(node.value, depth)}`;
-  case 'deleted':
-    return `${'    '.repeat(depth)}- ${key}: ${formatValue(node.value, depth)}`;
-  case 'changed':
-    return `${'    '.repeat(depth)}- ${key}: ${formatValue(node.value1, depth)}\n${'    '.repeat(depth)}+ ${key}: ${formatValue(node.value2, depth)}`;
-  case 'unchanged':
-    return `${'    '.repeat(depth)}  ${key}: ${formatValue(node.value, depth)}`;
-  case 'nested':
-    return `${'    '.repeat(depth)}  ${key}: {\n${stylish(node.children, depth + 1)}\n${'    '.repeat(depth)}  }`;
-  default:
-    throw new Error(`Unknown node type: ${node.type}`);
+    case 'added':
+      return `${indent}+ ${key}: ${formatValue(node.value, depth)}`;
+    case 'deleted':
+      return `${indent}- ${key}: ${formatValue(node.value, depth)}`;
+    case 'changed':
+      return `${indent}- ${key}: ${formatValue(node.value1, depth)}\n${indent}+ ${key}: ${formatValue(node.value2, depth)}`;
+    case 'unchanged':
+      return `${indent}  ${key}: ${formatValue(node.value, depth)}`;
+    case 'nested':
+      return `${indent}  ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}  }`;
+    default:
+      throw new Error(`Unknown node type: ${node.type}`);
   }
 };
 
@@ -23,15 +24,14 @@ const formatValue = (value, depth) => {
   }
   if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
     const formattedEntries = Object.entries(value)
-      .map(([key, val]) => `${'    '.repeat(depth + 1)}${key}: ${formatValue(val, depth + 1)}`)
+      .map(([key, val]) => `${'  '.repeat(depth)}${key}: ${formatValue(val, depth + 1)}`)
       .join('\n');
-    return `{\n${formattedEntries}\n${'    '.repeat(depth)}}`;
+    return `{\n${formattedEntries}\n${'  '.repeat(depth - 1)}}`; // Resta un nivel de profundidad
   }
   return value;
 };
 
-const stylish = (diff, depth = 1) => {
-  const indent = '    '.repeat(depth);
+const stylish = (diff, depth = 0) => {
   const result = diff.map((child) => formatNode(child, depth)).join('\n');
   return result;
 };
