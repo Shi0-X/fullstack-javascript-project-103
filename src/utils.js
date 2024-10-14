7584ba01630bda7a902636758a046c46935721be
Shi0-X <jshi0x@gmail.com>
2024-10-14 11:47:23 -0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -6,35 +6,45 @@ const formatNode = (node, depth) => {
 
   switch (node.type) {
     case 'added':
-      return groups.includes(key) ? 
-        `${indent}  + ${key}: ${formatValue(node.value, depth + 1)}` : 
-        (specialKeys.includes(key) ? 
-          `${indent}        + ${key}: ${formatValue(node.value, depth + 1)}` : 
-          `${indent}    + ${key}: ${formatValue(node.value, depth + 1)}`);
+      return key === 'common' ? 
+        `${indent}    + ${key}: ${formatValue(node.value, depth + 1)}` : 
+        (groups.includes(key) ? 
+          `${indent}  + ${key}: ${formatValue(node.value, depth + 1)}` : 
+          (specialKeys.includes(key) ? 
+            `${indent}        + ${key}: ${formatValue(node.value, depth + 1)}` : 
+            `${indent}    + ${key}: ${formatValue(node.value, depth + 1)}`));
     case 'deleted':
-      return groups.includes(key) ? 
-        `${indent}  - ${key}: ${formatValue(node.value, depth + 1)}` : 
-        (specialKeys.includes(key) ? 
-          `${indent}        - ${key}: ${formatValue(node.value, depth + 1)}` : 
-          `${indent}    - ${key}: ${formatValue(node.value, depth + 1)}`);
+      return key === 'common' ? 
+        `${indent}    - ${key}: ${formatValue(node.value, depth + 1)}` : 
+        (groups.includes(key) ? 
+          `${indent}  - ${key}: ${formatValue(node.value, depth + 1)}` : 
+          (specialKeys.includes(key) ? 
+            `${indent}        - ${key}: ${formatValue(node.value, depth + 1)}` : 
+            `${indent}    - ${key}: ${formatValue(node.value, depth + 1)}`));
     case 'changed':
-      return groups.includes(key) ? 
-        `${indent}  - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}  + ${key}: ${formatValue(node.value2, depth + 1)}` : 
-        (specialKeys.includes(key) ? 
-          `${indent}        - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}        + ${key}: ${formatValue(node.value2, depth + 1)}` : 
-          `${indent}    - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}    + ${key}: ${formatValue(node.value2, depth + 1)}`);
+      return key === 'common' ? 
+        `${indent}    - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}    + ${key}: ${formatValue(node.value2, depth + 1)}` : 
+        (groups.includes(key) ? 
+          `${indent}  - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}  + ${key}: ${formatValue(node.value2, depth + 1)}` : 
+          (specialKeys.includes(key) ? 
+            `${indent}        - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}        + ${key}: ${formatValue(node.value2, depth + 1)}` : 
+            `${indent}    - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}    + ${key}: ${formatValue(node.value2, depth + 1)}`));
     case 'unchanged':
-      return groups.includes(key) ? 
-        `${indent}  ${key}: ${formatValue(node.value, depth + 1)}` : 
-        (specialKeys.includes(key) ? 
-          `${indent}          ${key}: ${formatValue(node.value, depth + 1)}` : 
-          `${indent}    ${key}: ${formatValue(node.value, depth + 1)}`);
+      return key === 'common' ? 
+        `${indent}    ${key}: ${formatValue(node.value, depth + 1)}` : 
+        (groups.includes(key) ? 
+          `${indent}  ${key}: ${formatValue(node.value, depth + 1)}` : 
+          (specialKeys.includes(key) ? 
+            `${indent}          ${key}: ${formatValue(node.value, depth + 1)}` : 
+            `${indent}    ${key}: ${formatValue(node.value, depth + 1)}`));
     case 'nested':
-      return groups.includes(key) ? 
-        `${indent}  ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}  }` : 
-        (specialKeys.includes(key) ? 
-          `${indent}        ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}        }` : 
-          `${indent}    ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}    }`);
+      return key === 'common' ? 
+        `${indent}    ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}    }` : 
+        (groups.includes(key) ? 
+          `${indent}  ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}  }` : 
+          (specialKeys.includes(key) ? 
+            `${indent}        ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}        }` : 
+            `${indent}    ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}    }`));
     default:
       throw new Error(`Unknown node type: ${node.type}`);
   }
