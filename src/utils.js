b6ff5a9d96f79c8e7978c069e6e4033db771d481
Shi0-X <jshi0x@gmail.com>
2024-10-14 12:57:42 -0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -11,7 +11,7 @@ const formatValue = (value, depth) => {
   if (value === null) return 'null';
   if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
     const formattedEntries = Object.entries(value)
-      .map(([key, val]) => `${getIndent(depth + 1)}${key}: ${formatValue(val, depth + 0)}`)
+      .map(([key, val]) => `${getIndent(depth + 1)}${key}: ${formatValue(val, depth + 0)}`) //inferiores group 2 y 3
       .join('\n');
     return `{\n${formattedEntries}\n${getIndent(depth)}}`;
   }
@@ -24,21 +24,21 @@ const formatNode = (node, depth) => {
 
   switch (node.type) {
     case 'added':
-      return `${indent}+ ${key}: ${formatValue(node.value, depth + 0)}`;
+      return `${indent}+ ${key}: ${formatValue(node.value, depth + 0)}`; //setting5 y group3
     case 'deleted':
-      return `${indent}- ${key}: ${formatValue(node.value, depth + 0)}`;
+      return `${indent}- ${key}: ${formatValue(node.value, depth + 0)}`; //group 2
     case 'changed':
-      return `${indent}- ${key}: ${formatValue(node.value1, depth + 0)}\n${indent}+ ${key}: ${formatValue(node.value2, depth + 0)}`;
+      return `${indent}- ${key}: ${formatValue(node.value1, depth + 0)}\n${indent}+ ${key}: ${formatValue(node.value2, depth + 0)}`; //group1 key:value
     case 'unchanged':
       return `${indent} ${key}: ${formatValue(node.value, depth + 0)}`;
     case 'nested':
-      return `${indent} ${key}: {\n${node.children.map((child) => formatNode(child, depth + 1)).join('\n')}\n${indent} }`;
+      return `${indent} ${key}: {\n${node.children.map((child) => formatNode(child, depth + 2)).join('\n')}\n${indent} }`; //common y group 1
     default:
       throw new Error(`Unknown node type: ${node.type}`);
   }
 };
 
-const stylish = (diff, depth = 0) => {
+const stylish = (diff, depth = 1) => {
   return diff.map((node) => formatNode(node, depth)).join('\n');
 };
 
