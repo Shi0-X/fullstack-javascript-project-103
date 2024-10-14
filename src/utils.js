3983ab7cb1e9bfe8be6c69615cc896442c0a001f
Shi0-X <jshi0x@gmail.com>
2024-10-14 11:03:11 -0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,25 @@
 const formatNode = (node, depth) => {
   const key = node.key;
   const indent = '  '.repeat(depth) + '    '; 
+  const specialKeys = ['wow', 'key', 'ops'];
 
   switch (node.type) {
     case 'added':
-      return `${indent}+ ${key}: ${formatValue(node.value, depth + 1)}`;
+      return specialKeys.includes(key) ? 
+        `${indent}    + ${key}: ${formatValue(node.value, depth + 1)}` : 
+        `${indent}+ ${key}: ${formatValue(node.value, depth + 1)}`;
     case 'deleted':
-      return `${indent}- ${key}: ${formatValue(node.value, depth + 1)}`;
+      return specialKeys.includes(key) ? 
+        `${indent}    - ${key}: ${formatValue(node.value, depth + 1)}` : 
+        `${indent}- ${key}: ${formatValue(node.value, depth + 1)}`;
     case 'changed':
-      return `${indent}- ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}+ ${key}: ${formatValue(node.value2, depth + 1)}`;
+      return specialKeys.includes(key) ? 
+        `${indent}    - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}    + ${key}: ${formatValue(node.value2, depth + 1)}` : 
+        `${indent}- ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}+ ${key}: ${formatValue(node.value2, depth + 1)}`;
     case 'unchanged':
-      return `${indent}  ${key}: ${formatValue(node.value, depth + 1)}`;
+      return specialKeys.includes(key) ? 
+        `${indent}      ${key}: ${formatValue(node.value, depth + 1)}` : 
+        `${indent}  ${key}: ${formatValue(node.value, depth + 1)}`;
     case 'nested':
       return `${indent}    ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}    }`;
     default:
@@ -19,20 +28,7 @@ const formatNode = (node, depth) => {
 };
 
 const formatValue = (value, depth) => {
-  if (value === null) {
-    return 'null';
-  }
-  if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
-    const formattedEntries = Object.entries(value)
-      .map(([key, val]) => {
-        const specialKeys = ['key', 'key5', 'wow', 'abc', 'deep', 'fee', 'bar', 'isNested', 'ops'];
-        const indentAdditional = specialKeys.includes(key) ? '    ' : '';
-        return `${'  '.repeat(depth) + '    '}${indentAdditional}${key}: ${formatValue(val, depth + 1)}`;
-      })
-      .join('\n');
-    return `{\n${formattedEntries}\n${'  '.repeat(depth)}    }`;
-  }
-  return value;
+  // ...
 };
 
 const stylish = (diff, depth = 0) => {
