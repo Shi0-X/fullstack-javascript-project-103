cc24030f9e7201d3514d3170582573de316a7182
Shi0-X <jshi0x@gmail.com>
2024-10-14 11:35:42 -0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,39 @@
 const formatNode = (node, depth) => {
   const key = node.key;
-  const indent = '  '.repeat(depth) + '    '; 
+  const indent = '  '.repeat(depth); 
   const specialKeys = ['wow', 'key', 'ops'];
 
   switch (node.type) {
     case 'added':
-      return specialKeys.includes(key) ? 
+      return key === 'common' ? 
         `${indent}    + ${key}: ${formatValue(node.value, depth + 1)}` : 
-        `${indent}+ ${key}: ${formatValue(node.value, depth + 1)}`;
+        (specialKeys.includes(key) ? 
+          `${indent}        + ${key}: ${formatValue(node.value, depth + 1)}` : 
+          `${indent}    + ${key}: ${formatValue(node.value, depth + 1)}`);
     case 'deleted':
-      return specialKeys.includes(key) ? 
+      return key === 'common' ? 
         `${indent}    - ${key}: ${formatValue(node.value, depth + 1)}` : 
-        `${indent}- ${key}: ${formatValue(node.value, depth + 1)}`;
+        (specialKeys.includes(key) ? 
+          `${indent}        - ${key}: ${formatValue(node.value, depth + 1)}` : 
+          `${indent}    - ${key}: ${formatValue(node.value, depth + 1)}`);
     case 'changed':
-      return specialKeys.includes(key) ? 
+      return key === 'common' ? 
         `${indent}    - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}    + ${key}: ${formatValue(node.value2, depth + 1)}` : 
-        `${indent}- ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}+ ${key}: ${formatValue(node.value2, depth + 1)}`;
+        (specialKeys.includes(key) ? 
+          `${indent}        - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}        + ${key}: ${formatValue(node.value2, depth + 1)}` : 
+          `${indent}    - ${key}: ${formatValue(node.value1, depth + 1)}\n${indent}    + ${key}: ${formatValue(node.value2, depth + 1)}`);
     case 'unchanged':
-      return specialKeys.includes(key) ? 
-        `${indent}      ${key}: ${formatValue(node.value, depth + 1)}` : 
-        `${indent}  ${key}: ${formatValue(node.value, depth + 1)}`;
+      return key === 'common' ? 
+        `${indent}    ${key}: ${formatValue(node.value, depth + 1)}` : 
+        (specialKeys.includes(key) ? 
+          `${indent}          ${key}: ${formatValue(node.value, depth + 1)}` : 
+          `${indent}    ${key}: ${formatValue(node.value, depth + 1)}`);
     case 'nested':
-      return `${indent}    ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}    }`;
+      return key === 'common' ? 
+        `${indent}    ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}    }` : 
+        (specialKeys.includes(key) ? 
+          `${indent}        ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}        }` : 
+          `${indent}    ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}    }`);
     default:
       throw new Error(`Unknown node type: ${node.type}`);
   }
