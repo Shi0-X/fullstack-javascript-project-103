9d3a97fc718aea71b5d2fc8ffa3980186309729c
Shi0-X <jshi0x@gmail.com>
2024-10-24 11:06:18 -0500

    Revert "update"
    This reverts commit 6df7fe16625ed3abfb3d79b0f77d3ab66d688e03.

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 12, // ES2021
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-multi-spaces': ['error', { ignoreEOLComments: false }],
+      'no-trailing-spaces': 'error',
+      indent: ['error', 2],
+      'max-len': ['error', { code: 900 }], // Cambiado a 150 caracteres
+    },
+  },
+];
