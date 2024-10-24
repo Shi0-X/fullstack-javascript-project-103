a2de6372af22228c7ca5376baae03860df9e09de
Shi0-X <jshi0x@gmail.com>
2024-10-24 14:27:29 -0500

    Update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,20 +6,30 @@ const stylishResult = fs.readFileSync(getPath('stylish-result.txt'), 'utf8');
 const plainResult = fs.readFileSync(getPath('plain-result.txt'), 'utf8');
 const jsonResult = JSON.parse(fs.readFileSync(getPath('json-result.json'), 'utf8'));
 
+const generateFilePaths = (filename1, filename2, extension) => [
+  getPath(`${filename1}.${extension}`),
+  getPath(`${filename2}.${extension}`),
+];
+
+const testGenDiff = (filepath1, filepath2, format, expectedResult) => {
+  expect(genDiff(filepath1, filepath2, format)).toBe(expectedResult);
+};
+
+const testGenDiffJSON = (filepath1, filepath2, expectedResult) => {
+  expect(JSON.parse(genDiff(filepath1, filepath2, 'json'))).toEqual(expectedResult);
+};
+
 test('Format for stylish Result - YAML File', () => {
-  const filepath1 = getPath('file1-y.yaml');
-  const filepath2 = getPath('file2-y.yaml');
-  expect(genDiff(filepath1, filepath2, 'stylish')).toBe(stylishResult);
+  const [filepath1, filepath2] = generateFilePaths('file1-y', 'file2-y', 'yaml');
+  testGenDiff(filepath1, filepath2, 'stylish', stylishResult);
 });
 
 test('Format for plain Result - YAML File', () => {
-  const filepath1 = getPath('file1-y.yaml');
-  const filepath2 = getPath('file2-y.yaml');
-  expect(genDiff(filepath1, filepath2, 'plain')).toBe(plainResult);
+  const [filepath1, filepath2] = generateFilePaths('file1-y', 'file2-y', 'yaml');
+  testGenDiff(filepath1, filepath2, 'plain', plainResult);
 });
 
 test('Format for JSON Result - JSON File', () => {
-  const filepath1 = getPath('file1.json');
-  const filepath2 = getPath('file2.json');
-  expect(JSON.parse(genDiff(filepath1, filepath2, 'json'))).toEqual(jsonResult);
+  const [filepath1, filepath2] = generateFilePaths('file1', 'file2', 'json');
+  testGenDiffJSON(filepath1, filepath2, jsonResult);
 });
